58f13020e5a7f9bdef840f618c3362780216bf9c
Pritam Saha <saha.pritam.work@gmail.com>
2023-06-20 21:51:43 +0530

    Added a maven quickstart archetype project and added mysql connector dependency in pom file.

---
+++ jdbc-project/src/main/java/org/pritam/jdbc_project/App.java
@@ -0,0 +1,13 @@
+package org.pritam.jdbc_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
