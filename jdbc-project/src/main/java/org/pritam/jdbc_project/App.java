866aec62efb559bf9f380a7c61da31c7577d6973
Pritam Saha <saha.pritam.work@gmail.com>
2023-06-21 21:02:21 +0530

    Added insert option for customer and update option for customer by id only if id exists else nothing will be updated.

--- jdbc-project/src/main/java/org/pritam/jdbc_project/App.java
+++ jdbc-project/src/main/java/org/pritam/jdbc_project/App.java
@@ -1,5 +1,7 @@
 package org.pritam.jdbc_project;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
@@ -18,38 +20,67 @@ import org.pritam.jdbc_project.model.Customers;
 public class App {
 	public static void main(String[] args) throws Exception {
 		CustomersDAO customersDAO = new CustomersDAO();
-		Scanner sc = new Scanner(System.in);
-
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		while (true) {
 			System.out.println("\n1.Display All Customer" 
 							+ "\n2.Display Customer By ID" 
 							+ "\n3.Display Customer By City"
 							+ "\n4.Display Customer By Country" 
+							+ "\n5.Insert Customer"
+							+ "\n6.Update Customer"
 							+ "\n9.Exit");
 			System.out.print("Enter Your Choice :- ");
-			int n = sc.nextInt();
+			int n = Integer.parseInt(br.readLine());
 			try {
 				if (n == 1)
 					Customers.printCustomers(customersDAO.getCustomersBy(null, null));
 				else if (n == 2) {
 					System.out.print("Enter the Customer ID :- ");
-					int id = sc.nextInt();
+					int id = Integer.parseInt(br.readLine());
 					Customers.printCustomers(customersDAO.getCustomersBy("customerid", Integer.toString(id)));
 				}
 				else if (n == 3) {
 					System.out.print("Enter the City :- ");
-					if(sc.hasNextLine())
-						sc.nextLine();
-					String city = sc.nextLine();
+					String city = br.readLine();
 					Customers.printCustomers(customersDAO.getCustomersBy("city", city));
-				}else if (n == 4) {
+				}
+				else if (n == 4) {
 					System.out.print("Enter the Country :- ");
-					if(sc.hasNextLine())
-						sc.nextLine();
-					sc.reset();
-					String country = sc.nextLine();
+					String country = br.readLine();
 					Customers.printCustomers(customersDAO.getCustomersBy("country", country));
-				} else if (n == 9) {
+				}
+				else if(n==5 || n==6 ) {
+					int customerID=0;
+					if(n==6) {
+						System.out.print("Enter the ID of the customer whose data you want to update :- ");
+						customerID=Integer.parseInt(br.readLine());
+					}
+					
+					System.out.print((n==5)?"Enter the name :- ":"Enter the new name if you want to edit else type the existing name :- ");
+					String customerName=br.readLine();
+					
+					System.out.print((n==5)?"Enter the contact name :- ":"Enter the new contact name if you want to edit else type the existing contact name :- ");
+					String contactName=br.readLine();
+					
+					System.out.print((n==5)?"Enter the address :- ":"Enter the new address if you want to edit else type the existing address :- ");
+					String address=br.readLine();
+					
+					System.out.print((n==5)?"Enter the city :- ":"Enter the new city if you want to edit else type the existing city :- ");
+					String city=br.readLine();
+					
+					System.out.print((n==5)?"Enter the postal code :- ":"Enter the new postal code if you want to edit else type the existing postal code :- ");
+					String postalCode=br.readLine();
+					
+					System.out.print((n==5)?"Enter the country :- ":"Enter the new country if you want to edit else type the existing country :- ");
+					String country=br.readLine();
+					
+					Customers customer = new Customers(customerID,customerName,contactName,address,city,postalCode,country);
+					if(n==5)
+						customersDAO.insertCustomer(customer);
+					else
+						customersDAO.updateCustomer(customer);	
+				}
+				else if (n == 9) {
 					customersDAO.closeConnection();
 					break;
 				}
